b309f18a14154e6671d98e775b3d4f7684fdf631
SIMANTO SARKER <129303415+simantosarker@users.noreply.github.com>
2024-03-05 03:12:03 +0600

    Delete src directory

--- src/httpService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const iAxios = axios.create({
-  baseURL: "http://localhost:5000",
-  timeout: 5000,
-});
-
-export default iAxios;
--- src/pages/areaView.js
+++
@@ -1,7 +0,0 @@
-import AreaOfficerlList from "../components/areaOfficerList";
-
-const AreaView = () => {
-  return <AreaOfficerlList />;
-};
-
-export default AreaView;
